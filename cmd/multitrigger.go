df551f58c107eb9b011450de973242e032bed729
MorErel <dayan.mordi@gmail.com>
2024-02-28 16:23:24 +0200

    moved triggerbuilds

--- cmd/multitrigger.go
+++ cmd/multitrigger.go
@@ -1,10 +1,13 @@
 package cmd
 
 import (
+	"bbox/pkg/display"
 	"bbox/pkg/params"
+	"bbox/pkg/types"
 	"bbox/teamcity"
 	"net/url"
 	"os"
+	"sync"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -16,15 +19,6 @@ var multiTriggerCmdName = "multi-trigger"
 var multiArtifactsPath = "./"
 var waitForBuilds = true
 var waitTimeout = 15 * time.Minute
-
-type BuildResult struct {
-	BuildName           string
-	WebURL              string
-	BranchName          string
-	BuildStatus         string
-	DownloadedArtifacts bool
-	Error               error
-}
 
 var multiTriggerCmd = &cobra.Command{
 	Use:   multiTriggerCmdName,
@@ -47,8 +41,107 @@ var multiTriggerCmd = &cobra.Command{
 		}
 
 		client := teamcity.NewTeamCityClient(url, teamcityUsername, teamcityPassword)
-		client.TriggerBuilds(allCombinations, waitForBuilds, waitTimeout, multiArtifactsPath)
+		
+		triggerBuilds(client, allCombinations, waitForBuilds, waitTimeout, multiArtifactsPath)
 	},
+}
+
+// triggerBuilds triggers the builds for each set of build parameters, wait and download artifacts if needed using work group
+func triggerBuilds(c *teamcity.Client, params []types.BuildParameters, waitForBuilds bool, waitTimeout time.Duration, multiArtifactsPath string) {
+	buildFailed := false
+	resultsChan := make(chan types.BuildResult)
+
+	var wg sync.WaitGroup
+
+	for _, param := range params {
+		// Increment the WaitGroup's counter for each goroutine
+		wg.Add(1)
+
+		// Launch a goroutine for each set of parameters
+		go func(p types.BuildParameters) {
+			defer wg.Done() // Decrement the counter when the goroutine completes
+
+			log.WithFields(log.Fields{
+				"branchName":        p.BranchName,
+				"buildTypeId":       p.BuildTypeId,
+				"properties":        p.PropertiesFlag,
+				"downloadArtifacts": p.DownloadArtifacts,
+			}).Debug("triggering Build")
+
+			triggerResponse, err := c.Build.TriggerBuild(p.BuildTypeId, p.BranchName, p.PropertiesFlag)
+
+			if err != nil {
+				log.Error("error triggering build: ", err)
+				return
+			}
+
+			log.WithFields(log.Fields{
+				"buildName": triggerResponse.BuildType.Name,
+				"webURL":    triggerResponse.WebURL,
+			}).Info("Build Triggered")
+
+			downloadedArtifacts := false
+			status := "UNKNOWN"
+
+			if waitForBuilds {
+				log.Infof("waiting for build %s", triggerResponse.BuildType.Name)
+
+				build, err := c.Build.WaitForBuild(triggerResponse.BuildType.Name, triggerResponse.ID, waitTimeout)
+
+				if err != nil {
+					log.Errorf("error waiting for build %s: %s", triggerResponse.BuildType.Name, err.Error())
+				}
+
+				log.WithFields(log.Fields{
+					"buildStatus": build.Status,
+					"buildState":  build.State,
+				}).Infof("build %s Finished", triggerResponse.BuildType.Name)
+
+				if p.DownloadArtifacts && err == nil && c.Artifacts.BuildHasArtifact(build.ID) {
+					log.Infof("downloading Artifacts for %s", triggerResponse.BuildType.Name)
+
+					err = c.Artifacts.DownloadAndUnzipArtifacts(build.ID, p.BuildTypeId, multiArtifactsPath)
+					if err != nil {
+						log.Errorf("error downloading artifacts for build %s: %s", triggerResponse.BuildType.Name, err.Error())
+					}
+
+					downloadedArtifacts = err == nil
+				}
+
+				status = build.Status
+
+				if status != "SUCCESS" {
+					buildFailed = true
+				}
+			}
+
+			resultsChan <- types.BuildResult{
+				BuildName:           triggerResponse.BuildType.Name,
+				WebURL:              triggerResponse.WebURL,
+				BranchName:          p.BranchName,
+				BuildStatus:         status,
+				DownloadedArtifacts: downloadedArtifacts,
+				Error:               err,
+			}
+		}(param)
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultsChan)
+	}()
+
+	var results []types.BuildResult
+	for result := range resultsChan {
+		results = append(results, result)
+	}
+
+	display.ResultsTable(results)
+
+	if buildFailed {
+		log.Error("one or more builds failed, more info in links above")
+		os.Exit(2)
+	}
 }
 
 func init() {
--- teamcity/build.go
+++ teamcity/build.go
@@ -1,16 +1,13 @@
 package teamcity
 
 import (
-	"bbox/pkg/display"
 	"bbox/pkg/types"
 	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/pkg/errors"
 	"io"
-	"os"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/avast/retry-go/v4"
@@ -107,104 +104,6 @@ func (bs *BuildService) TriggerBuild(buildTypeId string, branchName string, para
 	}).Debug("triggered Response")
 
 	return triggerBuildResponse, nil
-}
-
-// TriggerBuilds triggers the builds for each set of build parameters, wait and download artifacts if needed using work group
-func (c *Client) TriggerBuilds(params []types.BuildParameters, waitForBuilds bool, waitTimeout time.Duration, multiArtifactsPath string) {
-	buildFailed := false
-	resultsChan := make(chan types.BuildResult)
-
-	var wg sync.WaitGroup
-
-	for _, param := range params {
-		// Increment the WaitGroup's counter for each goroutine
-		wg.Add(1)
-
-		// Launch a goroutine for each set of parameters
-		go func(p types.BuildParameters) {
-			defer wg.Done() // Decrement the counter when the goroutine completes
-
-			log.WithFields(log.Fields{
-				"branchName":        p.BranchName,
-				"buildTypeId":       p.BuildTypeId,
-				"properties":        p.PropertiesFlag,
-				"downloadArtifacts": p.DownloadArtifacts,
-			}).Debug("triggering Build")
-
-			triggerResponse, err := c.Build.TriggerBuild(p.BuildTypeId, p.BranchName, p.PropertiesFlag)
-
-			if err != nil {
-				log.Error("error triggering build: ", err)
-				return
-			}
-
-			log.WithFields(log.Fields{
-				"buildName": triggerResponse.BuildType.Name,
-				"webURL":    triggerResponse.WebURL,
-			}).Info("Build Triggered")
-
-			downloadedArtifacts := false
-			status := "UNKNOWN"
-
-			if waitForBuilds {
-				log.Infof("waiting for build %s", triggerResponse.BuildType.Name)
-
-				build, err := c.Build.WaitForBuild(triggerResponse.BuildType.Name, triggerResponse.ID, waitTimeout)
-
-				if err != nil {
-					log.Errorf("error waiting for build %s: %s", triggerResponse.BuildType.Name, err.Error())
-				}
-
-				log.WithFields(log.Fields{
-					"buildStatus": build.Status,
-					"buildState":  build.State,
-				}).Infof("build %s Finished", triggerResponse.BuildType.Name)
-
-				if p.DownloadArtifacts && err == nil && c.Artifacts.BuildHasArtifact(build.ID) {
-					log.Infof("downloading Artifacts for %s", triggerResponse.BuildType.Name)
-
-					err = c.Artifacts.DownloadAndUnzipArtifacts(build.ID, p.BuildTypeId, multiArtifactsPath)
-					if err != nil {
-						log.Errorf("error downloading artifacts for build %s: %s", triggerResponse.BuildType.Name, err.Error())
-					}
-
-					downloadedArtifacts = err == nil
-				}
-
-				status = build.Status
-
-				if status != "SUCCESS" {
-					buildFailed = true
-				}
-			}
-
-			resultsChan <- types.BuildResult{
-				BuildName:           triggerResponse.BuildType.Name,
-				WebURL:              triggerResponse.WebURL,
-				BranchName:          p.BranchName,
-				BuildStatus:         status,
-				DownloadedArtifacts: downloadedArtifacts,
-				Error:               err,
-			}
-		}(param)
-	}
-
-	go func() {
-		wg.Wait()
-		close(resultsChan)
-	}()
-
-	var results []types.BuildResult
-	for result := range resultsChan {
-		results = append(results, result)
-	}
-
-	display.ResultsTable(results)
-
-	if buildFailed {
-		log.Error("one or more builds failed, more info in links above")
-		os.Exit(2)
-	}
 }
 
 // WaitForBuild waits for a build to finish
