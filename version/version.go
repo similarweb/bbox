e78e12f6931965625d916e68a759a72ba9e1ef61
OzBena <109233461+OzBena@users.noreply.github.com>
2024-07-01 10:38:31 +0300

    Update version.go

--- version/version.go
+++ version/version.go
@@ -1,8 +1,18 @@
 package version
 
 // Set with LDFLAGS.
-var version = "unset-dev"
+var (
+	// Version of the release, the value injected by .goreleaser
+	version = `{{.Version}}`
 
+	// Commit hash of the release, the value injected by .goreleaser
+	commit = `{{.Commit}}`
+)
 func GetVersion() string {
 	return version
+}
+
+// GetFormattedVersion returns the current version and commit hash
+func GetFormattedVersion() string {
+	return fmt.Sprintf("%s (%s)", version, commit)
 }
